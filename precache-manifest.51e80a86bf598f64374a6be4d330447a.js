be30d5d4ab9d56ff4e740346afccb2d6b3867aaa
openrpc-bastion <openrpc-bastion@protonmail.com>
2019-08-18 07:50:37 +0000

    github pages release

---
+++ precache-manifest.51e80a86bf598f64374a6be4d330447a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "464195cff8dd8b97874500ea7921f3ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7eec824a75bd89cc4fd",
+    "url": "/static/css/main.e8c97df1.chunk.css"
+  },
+  {
+    "revision": "6d5b58946acfdd803283",
+    "url": "/static/js/2.16e7adbc.chunk.js"
+  },
+  {
+    "revision": "c7eec824a75bd89cc4fd",
+    "url": "/static/js/main.d90ed9eb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
