ad32811bac2cb0b06deec6c50d37bfa312caf146
openrpc-bastion <openrpc-bastion@protonmail.com>
2019-12-16 18:00:30 +0000

    github pages release

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.af82788e1eaa0980dc6e5554ab38ae5d.js"
+  "/precache-manifest.83edd236b3998900182b1153d1b0dd3e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
